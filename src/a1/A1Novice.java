045e66d51774dbd569504faac0fbcd0baf97fdf8
lyc <lyc@lycdemacbook.wireless-1x.unc.edu>
2019-08-25 21:36:34 -0400

    hahahahah

--- src/a1/A1Novice.java
+++ src/a1/A1Novice.java
@@ -9,6 +9,21 @@ public class A1Novice {
 		Scanner scan = new Scanner(System.in);
 
 		// Your code follows here.
-		
+		Double c=scan.nextDouble();
+		for (int i = 0; i<c; i ++) {
+			String x = scan.next();
+			String z = x.substring(0,1) + "." + scan.next()+":";
+			int y = scan.nextInt();
+			double d = 0;
+			for (int j=0; j<y; j++) {
+				int h = scan.nextInt();
+				x = scan.next();
+				double g = scan.nextDouble();
+				d = d + h * g;		
+			}
+			System.out.println(z + d);
+			
+		}
+		scan.close();
 	}
 }
