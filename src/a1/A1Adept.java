73b3ab5c00abb2665d194e87e5193b42daf266a5
lyc <lyc@lycdemacbook.wireless-1x.unc.edu>
2019-08-28 08:53:45 -0400

    hhahahahha

--- src/a1/A1Adept.java
+++ src/a1/A1Adept.java
@@ -7,8 +7,50 @@ public class A1Adept {
 	public static void main(String[] args) {
 		
 		Scanner scan = new Scanner(System.in);
-
+		int quantity = scan.nextInt();
 		// Your code follows here.
+		String[]names = new String[quantity];
+		Double[]prices = new Double[quantity];
+		for (int i=0;i <quantity; i++) {
+			names[i] = scan.next();
+			prices[i] = scan.nextDouble();
+		}
+		int numberofcost = scan.nextInt();
+		double Biggest = 0;
+		double Smallest = Integer.MAX_VALUE;
+		String rich = "";
+		String poor = "";
+		double total = 0;
 		
+		for (int j=0; j<numberofcost; j++) {
+			String customername = scan.next() +" "+ scan.next();//key
+			int listnumber = scan.nextInt();
+			Double Cost = 0.0;
+			for (int t=0;t<listnumber;t++) {	
+				int Qgood = scan.nextInt();
+				String namevariable = scan.next();
+				for (int h=0; h<names.length; h++) { 
+					if (names[h].equals(namevariable)) {//这里的input会再变吗
+					Cost = Cost + Qgood*prices[h];
+					// 这地方加重复 total = total + Cost;
+					} 	
+				 } 
+			 }
+			total = total + Cost;
+			if (Cost > Biggest) {
+					Biggest = Cost;
+				    rich = customername;
+				} 
+			if (Cost < Smallest ) {
+					Smallest = Cost;
+					poor = customername;
+			}
+		
+		 }
+		System.out.println ("Biggest: " + rich + " " + Biggest);
+		System.out.println ("Smallest: " + poor + " "+ Smallest);
+		System.out.println ("Average: " + total/numberofcost);
+		scan.close();
 	}
 }
+
--- src/a1/A1Jedi.java
+++ src/a1/A1Jedi.java
@@ -9,6 +9,41 @@ public class A1Jedi {
 		Scanner scan = new Scanner(System.in);
 
 		// Your code follows here.
+		int goodsnumber = scan.nextInt();
+		String[]names = new String[goodsnumber];
+		Double[]prices = new Double[goodsnumber];
+		int[]goodsindex = new int[goodsnumber];
+		int[] howmanybuyit = new int[goodsnumber];
+		for (int i=0;i<goodsnumber;i++) {
+			names[i] = scan.next();
+			prices[i] = scan.nextDouble();
+			goodsindex[i] = 0;
+		}
+		int customernumber = scan.nextInt();
+		for (int i=0; i<customernumber; i++) {
+			scan.next();
+			scan.next();
+			int listnumber = scan.nextInt();
+			for (int b=0; b < listnumber; b++) {
+				int quantity = scan.nextInt();//dismatch
+			    String namevariable = scan.next();
+				for (int a=0; a< names.length; a++) {
+					if (names[a].equals (namevariable)) {
+						goodsindex [a] = +quantity;
+						howmanybuyit [a]= + 1;
+					}
+				
+				}
+			}
 		
-	}
+		}
+		for (int i=0; i<goodsnumber; i++) {
+			if (howmanybuyit[i]== 0) {
+				System.out.println ("No customer bought" + names[i]);
+				
+			}
+			System.out.println (howmanybuyit[i] + " customers bought " + goodsindex[i] + " " + names[i] );
+		}
+		scan.close();
+}
 }
